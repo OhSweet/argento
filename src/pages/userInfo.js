b72bf4b797c3c7da394dbff7a499fc332b2fe1dd
mocanumarius <mariushtcwild@gmail.co>
2017-04-20 23:12:01 +0300

    ADDED QR !!

--- src/pages/userInfo.js
+++ src/pages/userInfo.js
@@ -1,77 +1,101 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
-import {Text, View, Navigator, TouchableHighlight, StyleSheet,TextInput} from 'react-native';
+import {
+	Text,
+	View,
+	Navigator,
+	TouchableHighlight,
+	StyleSheet,
+	TextInput,
+	Dimensions,
+	Image,
+	Button
+} from 'react-native';
+
+import Camera from "react-native-camera";
 
 class UserInfo extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      text: ""
-    }
-  }
+	constructor( props ) {
+		super( props );
+		this.state = {
+			parsedUrl: null,
+			bounds: {
+				origin: {
+					x: 0,
+					y: 0
+				},
+				size: {
+					height: 0,
+					width: 0
+				}
+			},
+			opacity: 0
+		};
+	}
 
-    render() {
-//tb sa am ceva ce sa returneze altfel navigatorul pune null. si da eroare aplicatia
-        return ( <View style={styles.container}>
-          <Text >Insert your age</Text>
-            <Text> This what you entered {this.state.text}</Text>
-          <TextInput style={styles.input}
-            onChangeText={ (text)=> {
-              //console.warn('hi', JSON.stringify(text))
-              return
-                this.setState({text})} }
-          value={this.state.text}/>
+	render( ) {
+		//tb sa am ceva ce sa returneze altfel navigatorul pune null. si da eroare aplicatia
+		return (
+			<View style={styles.container}>
+				<Camera
+					onBarCodeRead={this.readQR.bind( this )}
+					ref={( cam ) => {
+					this.camera = cam;
+				}}
+					style={styles.preview}
+					aspect={Camera.constants.Aspect.fill}>
+				</Camera>
 
+				<Image
+					style={{
+					opacity: this.state.opacity,
+					height: parseInt( 500 ),
+					width: Dimensions.get( 'window' ).width - 40,
+					left: parseInt( 20 ),
+					top: parseInt( 20 ),
+					resizeMode: 'contain',
+					position: 'absolute'
+				}}
+					source={{
+					uri: this.state.parsedUrl
+				}}/>
 
-          <Text style={styles.buttonText}>lalalala </Text>
-
-        </View>
-)
-    }
+			<Button onPress={() => { this.setState({ parsedUrl: ' ' })}} title="dismiss"></Button>
+			</View>
+		)
+	}
+	takePicture( ) {
+		this.camera.capture( ).then(( data ) => console.log( data )).catch(err => console.error( err ));
+	}
+	readQR( e ) {
+		if ( e.data) {
+			this.setState({ parsedUrl: e.data , opacity: 0.9 });
+		} else {
+			this.setState({ parsedUrl: null, opacity: 0 });
+		}
+	}
 
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#dddfd4'
-  },
-  titleText: {
-    flex: 5,
-    fontSize: 52,
-    fontFamily: 'HelveticaNeue-CondensedBold',
-    paddingTop: 40,
-    paddingRight: 20,
-    paddingLeft: 20,
-    color: '#173e43'
-  },
-  button: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    alignSelf: 'stretch',
-    backgroundColor: '#3fb0ac'
-  },
-  regularText: {
-    fontFamily: 'fantasy',
-    color: '#173e43'
-
-  },
-  buttonText: {
-    fontFamily: 'HelveticaNeue-CondensedBold',
-    color: 'black'
-  },
-  input: {
-    width: 100,
-    height: 40,
-    borderColor: '#173e43',
-    borderWidth: 2,
-    textAlign: 'center'
-
-  }
-
-})
+	container: {
+		flex: 1
+	},
+	preview: {
+		flex: 1,
+		justifyContent: 'flex-end',
+		alignItems: 'center',
+		height: Dimensions.get( 'window' ).height,
+		width: Dimensions.get( 'window' ).width
+	},
+	capture: {
+		flex: 0,
+		backgroundColor: '#fff',
+		borderRadius: 5,
+		color: '#000',
+		padding: 10,
+		margin: 40
+	}
+});
 
 export default UserInfo
