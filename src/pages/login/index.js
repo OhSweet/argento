5d75047dda8ee6745858aba01ab4f41f3badc4d0
Cristina <cristina.ionita13@yahoo.com>
2017-04-22 21:24:14 +0300

    somemessage

---
+++ src/pages/login/index.js
@@ -0,0 +1,22 @@
+import React, {PropTypes, Component} from 'react';
+import {
+    Text,
+    View,
+    Navigator,
+    TouchableHighlight,
+    Button,
+    StyleSheet
+} from 'react-native';
+import {FBLogin, FBLoginManager} from 'react-native-facebook-login';
+
+class Login extends Component {
+
+    render() {
+      console.warn(FBLoginManager)
+        return (
+      <Text/>
+        )
+    }
+
+}
+export default Login
