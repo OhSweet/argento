81b29657b51469454d6c869ec8edd286cb7c678e
mocanumarius <mariushtcwild@gmail.co>
2017-04-22 23:03:53 +0300

    Made login work

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -12,12 +12,42 @@ import { FBLogin, FBLoginManager } from 'react-native-facebook-login';
 class Login extends Component {
 
 	render( ) {
-
-		return (
-			<View style={{ flex: 1}}>
-			  <FBLogin>OK</FBLogin>
-			</View>
-		)
+		var _this = this;
+	      return (
+	        <FBLogin style={{ marginBottom: 10, }}
+	          ref={(fbLogin) => { this.fbLogin = fbLogin }}
+	          permissions={["email","user_friends"]}
+	          loginBehavior={FBLoginManager.LoginBehaviors.Native}
+	          onLogin={function(data){
+	            console.warn("Logged in!");
+	            console.warn(data);
+	            _this.setState({ user : data.credentials });
+	          }}
+	          onLogout={function(){
+	            console.warn("Logged out.");
+	            _this.setState({ user : null });
+	          }}
+	          onLoginFound={function(data){
+	            console.warn("Existing login found.");
+	            console.warn(data);
+	            _this.setState({ user : data.credentials });
+	          }}
+	          onLoginNotFound={function(){
+	            console.warn("No user logged in.");
+	            _this.setState({ user : null });
+	          }}
+	          onError={function(data){
+	            console.warn("ERROR");
+	            console.warn(data);
+	          }}
+	          onCancel={function(){
+	            console.warn("User cancelled.");
+	          }}
+	          onPermissionsMissing={function(data){
+	            console.warn("Check permissions!");
+	            console.warn(data);
+	          }}
+	        />)
 	}
 
 }
