3d0849c13c5c9a0f7d13ae548abe8b2ce6edc74e
mocanumarius <mariushtcwild@gmail.co>
2017-04-26 21:54:00 +0300

    changes tp scan and package json

--- src/pages/scan/index.js
+++ src/pages/scan/index.js
@@ -9,7 +9,8 @@ import {
 	TextInput,
 	Dimensions,
 	Image,
-	Button
+	Button,
+	PixelRatio
 } from 'react-native';
 
 import Camera from "react-native-camera";
@@ -19,18 +20,10 @@ class Scan extends Component {
 		super( props );
 		this.state = {
 			parsedUrl: null,
-			bounds: {
-				origin: {
-					x: 0,
-					y: 0
-				},
-				size: {
-					height: 0,
-					width: 0
-				}
-			},
 			opacity: 0
 		};
+		console.warn("dimesions are", JSON.stringify(Dimensions.get('window')))
+		console.warn("Pixel ratio is", PixelRatio.get("window"))
 	}
 
 	render( ) {
@@ -43,24 +36,31 @@ class Scan extends Component {
 					this.camera = cam;
 				}}
 					style={styles.preview}
-					aspect={Camera.constants.Aspect.fill}>
+					aspect={Camera.constants.Aspect.fit}>
 				</Camera>
+				{ this.state.parsedUrl? (
+					<Image
+						style={{
+						opacity: this.state.opacity,
+						height: this.state.bounds[0][1] - this.state.bounds[1][1],
+						width: this.state.bounds[2][0] - this.state.bounds[1][0],
+						top: this.state.bounds[1][1],
+						left: this.state.bounds[1][0],
+						resizeMode: 'contain',
+						position: 'absolute'
+					}}
+						source={{
+						uri: this.state.parsedUrl
+					}}/>
+				): null}
+			{ this.state.parsedUrl ? (
+				<View style={{ position: "absolute", top: 0, left: 0, right:0, bottom: 0	}}>
+					<Text style={{position:"absolute", bottom: 26, left: 0 , color: "white"}}> { this.state.bounds[0][0] + "|" + this.state.bounds[0][1] }</Text>
+					<Text style={{position:"absolute", top: 26, left: 0, color: "white"}}> { this.state.bounds[1][0] + "|" + this.state.bounds[1][1] }</Text>
+					<Text style={{position:"absolute", top: 30, right: 0, color: "white"}}> { this.state.bounds[2][0] + "|" + this.state.bounds[2][1] }</Text>
 
-				<Image
-					style={{
-					opacity: this.state.opacity,
-					height: parseInt( 500 ),
-					width: Dimensions.get( 'window' ).width - 40,
-					left: parseInt( 20 ),
-					top: parseInt( 20 ),
-					resizeMode: 'contain',
-					position: 'absolute'
-				}}
-					source={{
-					uri: this.state.parsedUrl
-				}}/>
-
-			<Button onPress={() => { this.setState({ parsedUrl: ' ' })}} title="dismiss"></Button>
+				</View>
+			): null }
 			</View>
 		)
 	}
@@ -68,8 +68,16 @@ class Scan extends Component {
 		this.camera.capture( ).then(( data ) => console.log( data )).catch(err => console.error( err ));
 	}
 	readQR( e ) {
+		let bounds = JSON.parse(e.bounds).map((bigArr) => bigArr.map(
+			(item, index) => {
+				return PixelRatio.roundToNearestPixel(item/3)
+				// Some random width ratio that works for me
+				if ( index == 0 ) return PixelRatio.roundToNearestPixel(item/3.10312)
+				}
+		))
+
 		if ( e.data) {
-			this.setState({ parsedUrl: e.data , opacity: 0.9 });
+			this.setState({ parsedUrl: e.data , opacity: 1 , bounds: bounds });
 		} else {
 			this.setState({ parsedUrl: null, opacity: 0 });
 		}
@@ -79,22 +87,17 @@ class Scan extends Component {
 
 const styles = StyleSheet.create({
 	container: {
-		flex: 1
+		flex: 1,
+		backgroundColor: "black"
 	},
 	preview: {
-		flex: 1,
-		justifyContent: 'flex-end',
-		alignItems: 'center',
-		height: Dimensions.get( 'window' ).height,
-		width: Dimensions.get( 'window' ).width
-	},
-	capture: {
-		flex: 0,
-		backgroundColor: '#fff',
-		borderRadius: 5,
-		color: '#000',
-		padding: 10,
-		margin: 40
+		position: "absolute",
+		top: 0,
+		bottom: 0,
+		left:0,
+		right: 0,
+		height: Dimensions.get( 'screen' ).height,
+		width: Dimensions.get( 'screen' ).width
 	}
 });
 
