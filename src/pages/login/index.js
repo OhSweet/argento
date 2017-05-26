01309ac8bdfc4fe6fe061244bcb3dd2220b2c12e
Cristina <cristina.ionita13@yahoo.com>
2017-05-26 11:06:40 +0300

    morning plugging

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,15 +1,24 @@
 import React, {PropTypes, Component} from 'react';
-import {Text,View,TouchableHighlight,Button,StyleSheet,Image} from 'react-native';
+import {
+    Text,
+    View,
+    TouchableHighlight,
+    Button,
+    StyleSheet,
+    Image,
+    Dimensions
+} from 'react-native';
 import {FBLogin, FBLoginManager} from 'react-native-facebook-login';
 import FBLoginView from './FBLoginView.js'
 import {identity} from "../../services/identity"
+import {Icon} from 'react-native-material-ui'
+const window = Dimensions.get('window');
 
 class Login extends Component {
     constructor(props) {
         super(props)
         this.state = {
             loggedIn: false,
-            profilePic: 'https://openclipart.org/download/247320/abstract-user-flat-4.svg',
             busy: true
         }
         this.checkIdentity.call(this)
@@ -19,7 +28,8 @@ class Login extends Component {
         identity.getToken().then((token) => {
             if (token) {
                 this.redirectToHome();
-            }
+            } else
+                this.setState({busy: false})
         })
     }
 
@@ -31,6 +41,7 @@ class Login extends Component {
         // console.log( "data is", data );
         identity.setToken(data.credentials.token);
         identity.setUserinfo(data.profile)
+        this.checkIdentity()
         // this.redirectToHome.call( this ) this.setState({ user: data, loggedIn: true, profilePic:
         // data.profile.picture.data.url });
     }
@@ -44,28 +55,46 @@ class Login extends Component {
     render() {
         var _this = this;
         return (
-            <View>
-                <View style={this.state.busy
-                    ? styles.fullPage
-                    : styles.noDisplay}><FBLogin
-								style={{
-								height: 300,
-								paddingTop: 50,
-								marginBottom: 0
-						}}
-								buttonView={< FBLoginView />}
-								ref={(fbLogin) => {
-								this.fbLogin = fbLogin
-						}}
-								permissions={["email", "user_friends"]}
-								loginBehavior={FBLoginManager.LoginBehaviors.Native}
-								onLogin={this.onLogin.bind(this)}
-								onLogout={this.onLogout.bind(this)}
-								onLoginFound={this.onLoginFound.bind(this)}
-								onLoginNotFound={this.onLoginNotFound.bind(this)}
-								onError={this.onLoginError.bind(this)}
-								onCancel={this.onLoginCancel.bind(this)}
-								onPermissionsMissing={this.onLoginError.bind(this)}/></View>
+            <View style={{
+                flex: 1
+            }}>
+                <View
+                    style={this.state.busy
+                    ? [styles.fullPage, styles.noDisplay]
+                    : styles.fullPage}>
+                    <FBLogin
+                        style={{
+                        height: 300,
+                        paddingTop: 50,
+                        marginBottom: 0
+                    }}
+                        buttonView={< FBLoginView />}
+                        ref={(fbLogin) => {
+                        this.fbLogin = fbLogin
+                    }}
+                        permissions={["email", "user_friends"]}
+                        loginBehavior={FBLoginManager.LoginBehaviors.Native}
+                        onLogin={this.onLogin.bind(this)}
+                        onLogout={this.onLogout.bind(this)}
+                        onLoginFound={this.onLoginFound.bind(this)}
+                        onLoginNotFound={this.onLoginNotFound.bind(this)}
+                        onError={this.onLoginError.bind(this)}
+                        onCancel={this.onLoginCancel.bind(this)}
+                        onPermissionsMissing={this.onLoginError.bind(this)}/>
+                </View>
+
+                <View
+                    style={this.state.busy
+                    ? [
+                        styles.fullPage, {justifyContent: 'center',
+                        alignItems: 'center'}
+                    ]
+                    : [
+                        styles.fullPage,
+                        styles.noDisplay
+                    ]}>
+                    <Text>Loading</Text>
+                </View>
 
             </View>
         )
@@ -73,17 +102,20 @@ class Login extends Component {
 }
 const styles = {
     noDisplay: {
-        width: 0,
-        height: 0
+        top: window.height,
+        bottom: -window.height
     },
     fullPage: {
         position: 'absolute',
+        justifyContent: 'space-between',
+        alignItems: 'stretch',
+        overflow: 'hidden',
         top: 0,
-        right: 0,
         left: 0,
-        bottom: 0,
-        backgroundColor: 'red'
+        right: 0,
+        bottom: 0
     }
+
 }
 
 export default Login
